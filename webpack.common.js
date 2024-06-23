951561f084785fa06b9d04a5717e5e242ed5b374
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-06-23 16:13:07 -0500

    Add index.html file to root directory and upated webpack dev.js settings and webpack.common.js settings

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Add this line
 
 module.exports = {
-    entry: {
-      app: './src/index.js',
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Todo List',
-        template: './index.html'
-      }),
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '',
+      template: './index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-    },
-    module: {
-     rules: [
-       {
-         test: /\.css$/i,
-         use: ['style-loader', 'css-loader'],
-       },
-       {
-         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-         type: 'asset/resource',
-       },
-       {
-         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-         type: 'asset/resource',
-       },
-     ],
-   },
-  };
+  },
+};
